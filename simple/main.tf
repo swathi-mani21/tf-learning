302d2020b0d7d8234270c23f68ca670c6e363df8
swathi-mani21 <112471693+swathi-mani21@users.noreply.github.com>
2023-03-23 14:58:09 +0530

    Update main.tf

--- simple/main.tf
+++ simple/main.tf
@@ -2,8 +2,7 @@ provider "aws"{
    region = "ap-south-1"
 }
 
-resource "aws_instance" "AWSEC2Instance" 
-{
+resource "aws_instance" "AWSEC2Instance"{
 ami = "ami-0e07dcaca348a0e68"
 instance_type = "t2.micro"
 security_group = "launch-wizard-2"
