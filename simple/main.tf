e28a04816e704d8e2ab6cac5e7bf3d76f1ee4b13
swathi-mani21 <docker55.mani@gmail.com>
2023-03-23 09:37:02 +0000

    ii

---
+++ simple/main.tf
@@ -0,0 +1,13 @@
+provider "aws"{
+   region = "ap-south-1"
+}
+resource "aws_instance" "AWSEC2Instance"{
+ami = "ami-0e07dcaca348a0e68"
+instance_type = "t2.micro"
+security_group = "launch-wizard-2"
+key_name  = "mani-key.pem"
+tags = {
+    Name = "EC2 Instance By Terraform"
+}
+}
+
