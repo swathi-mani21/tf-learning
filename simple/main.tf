e0f51951fb9c56fdf90dc1be661376a439616856
swathi-mani21 <112471693+swathi-mani21@users.noreply.github.com>
2023-03-24 13:04:04 +0530

    Update main.tf

--- simple/main.tf
+++ simple/main.tf
@@ -7,8 +7,3 @@ tags = {
   Name = "EC2 Instance By Terraform"
 }
 }
-provider "aws"{
-region = "ap-south-1"
-access_key = "AKIAQZZPUUQE64CYROPR"
-secret_key = "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"
-}
