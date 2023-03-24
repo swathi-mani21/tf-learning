a161248014746140fa991a105d641d60ccd0ea06
swathi-mani21 <112471693+swathi-mani21@users.noreply.github.com>
2023-03-24 11:45:52 +0530

    Delete output.tf

--- simple/output.tf
+++
@@ -1,9 +0,0 @@
-output "public-ip" {
-  description = "Public IP"
-  value       = aws_instance.terraform-cloud-vm.*.public_ip
-}
-
-output "publicdns" {
-  description = "URL EC2 Instance"
-  value       = aws_instance.terraform-cloud-vm.*.public_dns
-}
