491b943662389a7e3939b4271cacdf9dccf54325
swathi-mani21 <112471693+swathi-mani21@users.noreply.github.com>
2023-03-24 16:41:06 +0530

    Update provider.tf

--- simple/provider.tf
+++ simple/provider.tf
@@ -1,9 +1,6 @@
-terraform {
-  required_version = "~> 1.0" 
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.0"
-    }
-  }
+provider "aws"{
+  region = "ap-south-1"
+  access_key = "AKIAQZZPUUQEQOLZZTY3"
+  secret_key = "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"
 }
+
