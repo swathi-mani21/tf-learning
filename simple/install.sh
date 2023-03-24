dd823b127c6214ec7a273a859920ad09bafda421
swathi-mani21 <112471693+swathi-mani21@users.noreply.github.com>
2023-03-24 11:45:27 +0530

    Delete install.sh

--- simple/install.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-sudo yum update -y
-sudo yum install -y httpd
-sudo systemctl enable httpd
-sudo service httpd start  
-echo "<h1>This is terraform cloud demo" | sudo tee /var/www/html/index.html
