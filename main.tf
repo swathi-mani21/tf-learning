157bef65b169336f26a6a0563ae5a95a8254a1a1
swathi-mani21 <112471693+swathi-mani21@users.noreply.github.com>
2023-03-24 11:55:30 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ resources "aws_instance" "AWSEC2Instance"{
   ami = ""
   instance_type = "t2.micro"
   security_groups = ["  "]
-  key_name = ""
+  key_name = "mani-key.tf"
   tags = {
     Name = "EC2 Instance By Terraform"
   }
