f499f522a002d2a259850f6be2fe73a4f97c940b
swathi-mani21 <112471693+swathi-mani21@users.noreply.github.com>
2023-03-24 12:24:17 +0530

    Delete main.tf

--- main.tf
+++
@@ -1,9 +0,0 @@
-resources "aws_instance" "AWSEC2Instance"{
-  ami = "ami-0e07dcaca348a0e68"
-  instance_type = "t2.micro"
-  security_groups = ["launch-wizard-2"]
-  key_name = "mani-key.tf"
-  tags = {
-    Name = "EC2 Instance By Terraform"
-  }
-  }
