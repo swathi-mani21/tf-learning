101b94d928d47415f4d7d93e5f3781d17c28ae41
swathi-mani21 <112471693+swathi-mani21@users.noreply.github.com>
2023-03-24 13:00:42 +0530

    Update main.tf

--- simple/main.tf
+++ simple/main.tf
@@ -1,14 +1,14 @@
 resource "aws_instance" "AWSEC2Instance"{
-  ami = "ami-0e07dcaca348a0e68"
-  instance_type = "t2.micro"
-  security_groups = ["launch-wizard-1"]
-  key_name = "mani-key"
-  tags = {
-    Name = "EC2 Instance By Terraform"
-  }
-  }
+ami = "ami-0e07dcaca348a0e68"
+instance_type = "t2.micro"
+security_groups = ["launch-wizard-1"]
+key_name = "mani-key"
+tags = {
+  Name = "EC2 Instance By Terraform"
+}
+}
 provider "aws"{
-  region = "ap-south-1"
-  access_key = "AKIAQZZPUUQE64CYROPR"
-  secret_key = "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"
+region = "ap-south-1"
+access_key = "AKIAQZZPUUQE64CYROPR"
+secret_key = "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"
 }
