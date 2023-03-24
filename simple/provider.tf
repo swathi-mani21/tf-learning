ee1ac510905fa928f84041d96751ec5fde6991f7
swathi-mani21 <112471693+swathi-mani21@users.noreply.github.com>
2023-03-24 15:20:22 +0530

    Update provider.tf

--- simple/provider.tf
+++ simple/provider.tf
@@ -6,8 +6,4 @@ terraform {
       version = "~> 3.0"
     }
   }
-  region = "ap-south-1"
-  access_key = "AKIAQZZPUUQEYBR4RQI3"
-  secret_key = "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"
-  #profile = "default"
-}
+
